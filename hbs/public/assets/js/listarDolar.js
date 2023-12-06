e18b46d95afb18f0569a334dcc2539d604539c72
ortizj01 <juanestebanortiz70@gmail.com>
2023-12-06 14:23:40 -0500

    actualizaciones 2

--- hbs/public/assets/js/listarDolar.js
+++ hbs/public/assets/js/listarDolar.js
@@ -60,3 +60,110 @@ const listarUsuario = async() => {
 })
 })
  }
+
+ const actualizarUsuario = () => {
+    const nombres = document.getElementById('nombres').value
+    const apellidos = document.getElementById('apellidos').value
+    const documento = document.getElementById('documento').value
+    const correo = document.getElementById('correo').value
+    const telefono = document.getElementById('telefono').value
+    const edad = document.getElementById('edad').value
+    const direccion = document.getElementById('direccion').value
+    const precioDolar = document.getElementById('precioDolar').value
+    const password = document.getElementById('password').value
+
+    if (nombres.length == 0) {
+        document.getElementById('nombreHelp').innerHTML = 'Dato requerido'
+        
+    }
+
+    else if (apellidos.length == 0) {
+            document.getElementById('fechaAgendaHelp').innerHTML = 'Dato requerido'  
+        }
+
+    else if (documento.length == 0) {
+            document.getElementById('horaInicioHelp').innerHTML = 'Dato requerido'  
+        }
+
+    else if (correo.length == 0) {
+            document.getElementById('horaFinHelp').innerHTML = 'Dato requerido'  
+        }
+
+    else if (telefono.length == 0) {
+            document.getElementById('descripcionAgendaHelp').innerHTML = 'Dato requerido'  
+        }
+
+    else if (edad.length == 0) {
+            document.getElementById('estadoAgendaHelp').innerHTML = 'Dato requerido'  
+        }
+
+    else if (direccion.length == 0) {
+            document.getElementById('estadoAgendaHelp').innerHTML = 'Dato requerido'  
+        }
+
+    else if (precioDolar.length == 0) {
+            document.getElementById('estadoAgendaHelp').innerHTML = 'Dato requerido'  
+        }
+
+    else if (password.length == 0) {
+            document.getElementById('estadoAgendaHelp').innerHTML = 'Dato requerido'  
+        }
+
+    else {
+        let usuario = {
+            nombres: nombres,
+            apellidos: apellidos,
+            documento: documento,
+            correo: correo,
+            telefono: telefono,
+            edad: edad,
+            direccion: direccion,
+            precioDolar: precioDolar,
+            password: password
+        }
+        alert(JSON.stringify(usuario));
+        fetch(url, {
+            method: 'PUT',
+            mode: 'cors', // cross origin resource sharing
+            body: JSON.stringify(usuario), //convertir el objeto a JSON
+            headers: {"Content-type": "application/json; charset=UTF-8"} // escabezado de la solicitud
+        })
+        
+        .then((res) => res.json()) //obtener respuesta de la peticion
+        .then(json => {
+            alert(json.msg)
+        })
+    }
+
+}
+
+const redireccionarEditar = (objetoUsuario) => {
+    document.location.href = 'editar-usuario?usuario='+objetoUsuario
+}
+
+const editarUsuario = () => {
+
+    //obetener datos de la url
+    var urlParams = new URLSearchParams(window.location.search);
+    
+    // Asignar valores a la caja de texto
+    document.getElementById('nombres').value = urlParams.get('nombres')
+    document.getElementById('apellidos').value = urlParams.get('apellidos')
+    document.getElementById('documento').value = urlParams.get('documento')
+    document.getElementById('correo').value = urlParams.get('correo')
+    document.getElementById('telefono').value = urlParams.get('telefono')
+    document.getElementById('edad').value = urlParams.get('edad')
+    document.getElementById('direccion').value = urlParams.get('direccion')
+    document.getElementById('precioDolar').value = urlParams.get('precioDolar')
+    document.getElementById('password').value = urlParams.get('password')
+}
+
+if (document.querySelector('#btnRegistrar')) { //si el objeto existe
+    document.querySelector('#btnRegistrar')
+    .addEventListener('click', registrarUsuario)
+}
+
+if (document.querySelector('#btnActualizar')) {
+    document.querySelector('#btnActualizar')
+    .addEventListener('click', actualizarUsuario ) 
+}
